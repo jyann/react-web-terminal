85a3d6346524c07d4d73f08a3b4e442df3a39f1e
Jonathan Yannes <jonathan@sourcetoad.com>
2017-02-13 16:47:48 -0500

    reformatted some code

--- src/components/react-web-terminal/react-web-terminal.jsx
+++ src/components/react-web-terminal/react-web-terminal.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 import {getKey} from './key-prop-support';
 
-const nonCharKeys = ['Backspace', 'Tab', 'Clear', 'Enter', 'Shift', 'Control', 'Alt', 'CapsLock', 'Escape', 'PageUp', 'PageDown', 'End', 'Home', 'Delete', 'Meta', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19'];
+const nonCharKeys = [
+    'Backspace', 'Tab', 'Clear', 'Enter', 'Shift', 'Control', 'Alt', 'CapsLock', 'Escape', 'PageUp', 'PageDown', 'End', 'Home', 'Delete', 'Meta',
+    'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19'
+];
 
 export default class WebTerminal extends React.Component {
   // TODO add inline styles so they can be changed dynamically
